e4e0e5dbcb322124190e72afbfc553fc6640f83c
Rishabh Joshi <rishabh.joshi260905@gmail.com>
2026-02-03 10:31:33 +0530

    Initial deployment setup with Vercel config, 404 page, and ignore rules

--- job_portal/wsgi.py
+++ job_portal/wsgi.py
@@ -8,8 +8,14 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+
+# Load .env file
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    pass
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'job_portal.settings')
 
